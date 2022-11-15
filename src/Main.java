3dd44df4aaedbf23e3413c8bcc330b4ec889558f
otusajo <Olapejo1@umbc.edu>
2022-11-14 22:18:04 -0500

    new 11/14-Olapeju

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+import java.util.Random;
 public class Main {
+    static void introPage() {
+        //
+        System.out.println("Hello! Welcome to the Budgeting app! Let's get started with a username!");
+        System.out.println("Please enter your first name:");
+        Scanner nameF = new Scanner(System.in);
+        String firstName = nameF.next();
+        System.out.println("Please enter your last name:");
+        Scanner nameL = new Scanner(System.in);
+        String lastName = nameL.next();
+        Random num= new Random();
+        int ranNum=num.nextInt(99);
+        String userName= firstName.toUpperCase() + lastName.toUpperCase().substring(lastName.length() -1 )+ranNum;
+        System.out.println("Your username is: " +userName);
+        System.out.println("Hi " + firstName + ", let's start budgeting!");
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!1");
+        introPage();
+        new percentageBreakdown();
+        new enterBudget();
     }
 }
